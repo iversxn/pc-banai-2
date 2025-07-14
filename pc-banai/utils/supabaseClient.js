0ed60cd52da287da9d076b1c79cf589fd2c0dc30
Farhan Faiyaz <135104278+iversxn@users.noreply.github.com>
2025-07-15 04:06:44 +0600

    Update supabaseClient.js

--- pc-banai/utils/supabaseClient.js
+++ pc-banai/utils/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.POSTGRES_URL,
-  process.env.POSTGRES_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 export default supabase
