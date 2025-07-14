6caaf5ffad289807d64bd6b02f92c583235c9e77
Farhan Faiyaz <135104278+iversxn@users.noreply.github.com>
2025-07-15 03:54:31 +0600

    Update supabaseClient.js

--- pc-banai/utils/supabaseClient.js
+++ pc-banai/utils/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.postgres.gsjfvxlyjjprisfskhfz:Higalaxy3!@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzamZ2eGx5ampwcmlzZnNraGZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjI2NTAxNiwiZXhwIjoyMDY3ODQxMDE2fQ.B7DDRQCToCGsGJ1Rd-YibiXu4au5SBemH0pJ9E8HWuM
+  process.env.POSTGRES_URL,
+  process.env.POSTGRES_ANON_KEY
 )
 
 export default supabase
