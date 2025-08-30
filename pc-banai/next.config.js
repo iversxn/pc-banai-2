c5f75ef2cfa290086464dde5647262baf55f8605
Farhan Faiyaz <135104278+iversxn@users.noreply.github.com>
2025-08-30 22:12:28 +0600

    Update next.config.js

--- pc-banai/next.config.js
+++ pc-banai/next.config.js
@@ -1,7 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true,
+    serverActions: {},
   },
 }
 
