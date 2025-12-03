334bcf5059a4ec741f4b8d67381ea573781c9010
Farhan Faiyaz <135104278+iversxn@users.noreply.github.com>
2025-12-03 20:38:04 +0600

    Create supabaseServer.ts

---
+++ pc-banai/utils/supabaseServer.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+if (!url || !key) {
+  throw new Error("Missing Supabase server credentials");
+}
+
+export const supabaseServer = createClient(url, key, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false,
+  },
+});
